38746e77abc4be4d13e9a6eeb3b7db831a5d47d1
paccojl <paccojl@gmail.com>
2019-06-26 21:53:37 +0500

    Commit win/lose command implemented Elo calculation implemented Small fixes

--- sd2bot.js
+++ sd2bot.js
@@ -18,93 +18,183 @@ client.on('ready' , () => {
 
 
 function process(message) {
-	if(message.content.startsWith("setup")){
-		setup(message);
-	}
-
-	if(message.content.startsWith("test")){
-		
-		test(message);
-	}
-
-	if(message.content.startsWith("reg")){
-		sql.register(message);
-	}
-	if(message.content.startsWith("unreg")){
-		sql.unreg(message);
+	if(message.channel.type === 'text'){
+		if(message.content.startsWith("$setup")){
+			setup(message);
+		}
+	
+		if(message.content.startsWith("test")){
+			test(message);
+		}
+	
+		if(message.content.startsWith("$reg")){
+			sql.register(message);
+		}
+		if(message.content.startsWith("unreg")){
+			sql.unreg(message);
+		}
+		if(message.content.startsWith("$commitWin")){
+			commit(message,1);
+		}
+		if(message.content.startsWith("$commitLose")){
+			commit(message,0);
+		}
+		if(message.content.startsWith("$stats")){
+			sql.getStats(message,message.author);
+		}
 	}
 }
 
 
 
 async function test(message){
-	
+	//sql.recalcRating();
 }
+
 
 async function setup(message){
 
-	let firstPlayer;
-	let secondPlayer;
-
-	if(message.mentions.users.size >= 2){
-		firstPlayer = 	message.mentions.members.first(1)[0];
-		secondPlayer = message.mentions.members.first(2)[1];
-	} else {
-		message.reply("Нужно упомянуть 2 пользователей");
+	let size = Math.ceil(message.mentions.users.array().length / 2);
+	if(size>4){
+		message.reply(`максимальный размер матча 4 на 4`);
+		return;
+	}
+	if(!message.mentions.users.has(message.author.id)){
+		message.reply(`Необходимо упомянуть себя, только администраторы могут создавать матчи без своего участия`);
+	}
+	let teamOne;
+	let teamTwo;
+	if(message.mentions.users.size == size*2){
+		teamOne = message.mentions.users.array().slice(0,size);
+		teamTwo = message.mentions.users.array().slice(size);
+	} else{
+		message.reply(`Необходимо упомянуть ${size*2} пользователей`);
 		return;
 	}
 	
+
 	try{
-		if(!await Util.confirm(message,firstPlayer)){
-			message.reply(`<@${firstPlayer.id}> не подтвердил матч`)
-			return;
+
+		//проверить все ли игроки могут участвовать в матче(зарегестрированны и нет текущего матча)
+		await sql.canPlay(message.mentions.users.array());
+
+		//запросить подтверждение всех игроков
+		await Util.confirm(message,message.mentions.users.array(),`Подтвердите участие в матче
+${teamOne.map(u=>{return `<@${u.id}>`}).join(' ')}
+против
+${teamTwo.map(u=>{return `<@${u.id}>`}).join(' ')}` ,60000);
+
+		//определить стороны случайным образом	
+		let axisTeam;
+		let alliesTeam;
+
+		if(Util.random([0,1]) === 0){
+			axisTeam = teamOne;
+			alliesTeam = teamTwo;
+		} else {
+			alliesTeam = teamOne;
+			axisTeam = teamTwo;
 		}
-		if(!await Util.confirm(message,secondPlayer)){
-			message.reply(`<@${secondPlayer.id}> не подтвердил матч`)
-			return;
+
+		message.channel.send(`Союзники: ${alliesTeam.map(u=>{return `<@${u.id}>`}).join(' ')}
+Ось: ${axisTeam.map(u=>{return `<@${u.id}>`}).join(' ')}`);
+
+		//провести баны карт
+		
+		let maps = mapsConst[size*2];
+		let mapBans = new Array(0);
+
+		if(size < 4){
+			let answer = await Util.select(message,maps,2,alliesTeam);
+			maps = maps.filter(x=> answer.indexOf(x)<0);
+			mapBans.push(answer);
+
+			answer = await Util.select(message,maps,2,axisTeam);
+			maps = maps.filter(x=> answer.indexOf(x)<0);
+			mapBans.push(answer);
 		}
 		
-		let firstSide = Util.random([0,1]);
-
-		let axisUser;
-		let alliesUser;
-
-		if(firstSide = 0){
-			axisUser = firstPlayer;
-			alliesUser = secondPlayer;
-		} else {
-			alliesUser = firstPlayer;
-			axisUser = secondPlayer;
-		}
-
-		message.channel.send(`Союзники :<@${alliesUser.id}> \nОсь: <@${axisUser.id}>`);
-
-		let maps = mapsConst;
-		let answer = await Util.banSelect(message,maps,2,alliesUser);
-		maps = maps.filter(x=> answer.indexOf(x)<0);
-		answer = await Util.banSelect(message,maps,2,axisUser);
-		maps = maps.filter(x=> answer.indexOf(x)<0);
-
+		//выбрать случайную из оставшихся
 		let map = Util.random(maps);
 
-		message.channel.send(`<@${alliesUser.id}> <@${axisUser.id}> Случайная карта: ${map}`);
+		message.channel.send(`Случайная карта: ${map}`);
 
-		let alliesBan = await Util.banSelect(message,alliesConst,2,axisUser);
-		let axisBan = await Util.banSelect(message,axisConst,2,alliesUser,"Забань");
+		//баны дивизий
+		let alliesBan = await Util.select(message, alliesConst, 2, axisTeam);
+		let axisBan = await Util.select(message, axisConst, 2, alliesTeam);
 
-		//allies pick division then axis picks counterpick
-		let alliesList = alliesConst.filter(x => alliesBan.indexOf(x)<0);
-		let axisList = axisConst.filter(x => axisBan.indexOf(x)<0);
-		let alliesDiv = await Util.select(message,alliesConst,1,alliesUser);
-		let axisDiv = await Util.select(message,axisList,1,axisUser);
-		
+		let alliesAvalibleList = alliesConst.filter(x => alliesBan.indexOf(x)<0);
+		let axisAvalibleList = axisConst.filter(x => axisBan.indexOf(x)<0);
+
+		let sidesArray = Array(0);
+
+		//выбор дивизий
+		for(let i=0;i<size;i++){
+			let div = await Util.select(message, axisAvalibleList, 1, [axisTeam[i]] );
+			
+			let div2 = await Util.select(message, alliesAvalibleList, 1, [alliesTeam[i]] );
+
+			sidesArray.push({ id: axisTeam[i].id, side: 1, division: div[0] });
+			sidesArray.push({ id: alliesTeam[i].id, side: 0, division: div2[0] });
+		}
+
+	
+		//матч собран
+		//записать данные о матче, сторонах, банах и заблокировать игроков в базе данных
+
+		let matchid = await sql.regMatch(size, sidesArray, mapsConst[size*2].indexOf(map) ,mapBans, alliesBan, axisBan);
+
+		//вывести информацию о собранном матче
+
+		message.channel.send(`Матч №${matchid}
+${size}v${size}
+Карта: ${map}
+Союзники: 
+${sidesArray.filter(e=>{return e.side === 0}).map(e=>{return `<@${e.id}> : ${e.division}`}).join(`\n`)}
+Ось: 
+${sidesArray.filter(e=>{return e.side === 1}).map(e=>{return `<@${e.id}> : ${e.division}`}).join(`\n`)}
+Для подтверждения результатов матча используйте команду $commitWin или $commitLose`);
 	} catch(err) {
-		message.reply(`${err}, матч отменён`);
+		message.channel.send(`${err}, матч отменён`);
 		return;
 	}
+}
 
-	message.channel.send(`Матч <@${alliesUser.id}> против <@${axisUser.id}> \n
-	Карта: ${map}\n <@${alliesUser.id}> ${alliesDiv}\n <@${axisUser.id}> ${axisDiv}`);
+
+async function commit(message,wl){
+
+	try{
+		let matchid = await sql.getPlayerCurrentMatch(message.author);
+		if(matchid != null){
+
+			let players = await sql.getMatchSides(matchid);
+			let authorSide = players.find(player => player.id === message.author.id).side;
+
+			let result =!wl^authorSide; // 0 = allies wdin , 1= axis win
+
+			let playersToConfirm = players.filter(p => p.side != authorSide);
+
+			let description = `${playersToConfirm.map(p=>{return `<@${p.id}>`}).join(` `)}\n Вы ${wl==1?'проиграли':'победили'} в матче №${matchid}, подтвердите`;
+
+			if(await Util.confirm(message,playersToConfirm,description,60000,1)){
+
+				sql.commit(matchid,result);
+
+				let eloChanges = await sql.updateRating(players.filter(p => p.side==0),players.filter(p=>p.side == 1), result, players.length/2 );
+
+				message.channel.send(`Результат матча №${matchid} подтверждён!
+Выигравшая сторона: ${result==0?'Союзники':'Ось'}
+Изменения в рейтинге:
+${eloChanges.map(p =>{return `<@${p.id}> ${p.elo} => ${p.newElo}`}).join('\n')}`);
+
+			}
+
+		} else {
+			message.reply("не найден текущий матч");
+		}
+	} catch (err){
+		message.reply(err);
+	}
 }
 
 client.on("message", process);
--- sql.js
+++ sql.js
@@ -1,47 +1,169 @@
 const sqlite3 = require('sqlite3').verbose();
+const tables = require("./tables");
+const mapsConst = tables.maps;
+const divConst = [tables.allydivs, tables.axisdivs];
+
 
 async function register(message){
     let user = message.author;
     try{
-        await runQuery(`insert into players(id) values (${user.id})`);
+        await runQuery(`insert into players(id,elo1v1,elo2v2,eloTeam) values (${user.id},1500,1500,1500)`);
+        message.channel.send(`<@${user.id}> добро пожаловать!`);
     } catch (err){
-        if(err){ 
-            if(err.message === "SQLITE_CONSTRAINT: UNIQUE constraint failed: players.id"){
-                message.channel.send(`<@${user.id}> уже зарегистрирован!`);
-            } else {
-                message.channel.send(err.message);
-            }
-        } else{
-            message.channel.send(`<@${user.id}> добро пожаловать!`);
+        if(err.message === "SQLITE_CONSTRAINT: UNIQUE constraint failed: players.id"){
+            message.channel.send(`<@${user.id}> уже зарегистрирован!`);
+        } else {
+            message.channel.send(err.message);
         }
     }
 } 
 
-async function regMatch(players,map){
-    let matchId = await runQuery(`insert into matches(map) values (${map})`);
+async function regMatch(size,sides,map,mapBans,alliesBans,axisBans){
+    let matchId = await runQuery(`insert into matches(map,size) values (${map},${size})`);
 
-    let sidesvalues = players.map((p)=>{return `(${match.id},${p.id},${p.side},${p.division})`}).join(',');
+    let sidesvalues = sides.map((p)=>{return `(${matchId},${p.id},${p.side},${divConst[p.side].indexOf(p.division)})`}).join(',');
 
     runQuery(`insert into sides(matchid,playerid,side,division) values ${sidesvalues}`);
 
+    let mapbanvalues = mapBans.map(mb=>{return `(${matchId},${mapsConst[size*2].indexOf(mb)})`}).join(`,`);
+
+    runQuery(`insert into mapbans(matchid, map) values ${mapbanvalues}`);
+
+    let alliesbanvalues = alliesBans.map(db=>{return `(${matchId},${divConst[0].indexOf(db)})`}).join(`,`);
+    let axisbanvalues = axisBans.map(db => {return `(${matchId},${divConst[1].indexOf(db)})`}).join(`,`);
+
+    runQuery(`insert into divbans(matchid,division) values ${alliesbanvalues},${axisbanvalues}`);
+    
+    for(player of sides){
+        setPlayerCurrentMatch(player,matchId);
+    }
+
     return matchId;
 }
 
-async function getMatch(matchId){
-    match = select1Query(`select * from matches where id = ${matchId}`);
-    players = selectQuery(`select * from sides where matchid = ${matchId}`);
-    return [await match,await players];
+async function getMatchSides(matchId){
+    let match = await select1Query(`select size from matches where rowid = ${matchId}`);
+    let eloname = (match.size == 1)?`elo1v1`:(match.size==2)?`elo2v2`:`eloTeam`;          
+    let players = await selectQuery(`select cast(playerid as text) as id, side, division, ${eloname} as elo from sides join players on sides.playerid = players.id where matchid = ${matchId}`);
+    return await players;
 
 }
 
+async function canPlay(users){
+    for(user of users){
+        let matchId = await getPlayerCurrentMatch(user);
+        if (matchId != null){
+            throw `<@${user.id}> участвует в матче №${matchId} и не может участвовать в других матчах до подтверждения результатов`;
+        }
+    }
+    return true;
+}
 
 async function getPlayerCurrentMatch(user){
-    let matchId = (await select1Query(`select currentmatch from players where id = ${user.id}`)).currentmatch;
-    return matchId;
+    try{
+        let matchId = (await select1Query(`select currentmatch from players where id = ${user.id}`)).currentmatch;
+        return matchId;
+    } catch (err){
+        throw `<@${user.id}> не зарегистрирован`;
+    }
+    
+    
 }
 
-function setPlayerCurrentMatch(user,matchid){
-    runQuery(`update players set currentmatch = ${matchid} where id = ${user.id}`);
+async function setPlayerCurrentMatch(user,matchid){
+    try{
+       await runQuery(`update players set currentmatch = ${matchid} where id = ${user.id}`);
+    } catch (err){
+        throw `<@${user.id}> не зарегистрирован`;
+    }
+
+}
+
+function calcElo(playerElo,OpponentElo,result,K){
+    let q = 1/(1+10 ** ((OpponentElo - playerElo)/400));
+    return (playerElo + (K*(result - q))).toFixed(3);
+}
+
+async function getElo(user,size){
+    let eloname = (size == 1)?`elo1v1`:(size==2)?`elo2v2`:`eloTeam`; 
+    return await select1Query(`select cast(id as text), ${eloname} from players where id = ${user.id}`);
+}
+
+async function setElo(user,size,newElo){
+    let eloname = (size == 1)?`elo1v1`:(size==2)?`elo2v2`:`eloTeam`;  
+    runQuery(`update players set ${eloname} = ${newElo} where id = ${user.id}`);
+}
+
+async function commit(matchid,result){
+    await runQuery(`update matches set result = ${result}, date = datetime('now') where rowid = ${matchid}`);
+    await runQuery(`update players set currentmatch = null where currentmatch = ${matchid}`);
+}
+
+async function getStats(message,user){
+    let main = await select1Query(`select * from players where id = ${user.id}`);
+    let games = await selectQuery(`select * from sides join matches on matches.rowid = sides.matchid where sides.playerid = ${user.id}; `);
+    let gamesCount = games.length;
+    let gamesCount1v1 = games.filter(g=>g.size == 1).length;
+    let gamesCount2v2 = games.filter(g=>g.size == 2).length;
+    let gamesCountTeam = games.filter(g=>g.size > 2).length;
+    let gamesWin1v1 = games.filter(g=> g.side == g.result && g.size == 1).length;
+    let gamesWin2v2 = games.filter(g=> g.side == g.result && g.size == 2).length;
+    let gamesWinTeam = games.filter(g=> g.side == g.result && g.size > 2).length;
+    message.reply(`
+Всего игр: ${gamesCount}
+Текущий матч: ${main.currentmatch==null?'нет':`%${main.currentmatch}`}
+---1v1---
+ELO: ${main.elo1v1}
+Побед: ${gamesWin1v1}
+Поражений: ${gamesCount1v1-gamesWin1v1}
+%: ${(gamesWin1v1/gamesCount1v1)*100}%
+---2v2---
+ELO: ${main.elo2v2}
+Побед: ${gamesWin2v2}
+Поражений: ${gamesCount2v2-gamesWin2v2}
+%: ${(gamesWin2v2/gamesCount2v2)*100}%
+---Коммандные---
+ELO: ${main.eloTeam}
+Побед: ${gamesWinTeam}
+Поражений: ${gamesCountTeam-gamesWinTeam}
+%: ${(gamesWinTeam/gamesCountTeam)*100}%
+`)
+}
+
+async function updateRating(allies,axis,result,size){ 
+
+
+
+    let aliesaverage = allies.reduce((acc,p)=> acc+p.elo,0) / allies.length;    
+    let axisaverage = axis.reduce((acc,p)=> acc+p.elo,0) / axis.length;
+
+    let K = 30 * (1500/((aliesaverage+axisaverage)/2));
+
+    let newaxis = axis.map(p => {
+        return {id:p.id,  elo: p.elo, newElo : calcElo(p.elo,aliesaverage,result,K)};
+    });
+    let newallies = allies.map(p => {
+        return {id:p.id, elo: p.elo, newElo : calcElo(p.elo,axisaverage,result^1,K)};
+    });
+
+    for(player of [...newaxis,...newallies]){
+        setElo(player,size,player.newElo);
+    }
+    return [...newaxis,...newallies];
+    
+}
+
+async function recalcRating(){
+    let games = await selectQuery(`select rowid as id, result from matches where result IS NOT NULL order by datetime(date)`);
+    await runQuery(`update players set elo1v1 = 1500, elo2v2 = 1500, eloTeam = 1500`);
+    
+
+    for(game of games){
+        console.log(game.id);
+        let players = await getMatchSides(game.id);
+        let change = await updateRating(players.filter(p => p.side==0),players.filter(p=>p.side == 1), game.result, players.length/2 );
+        console.log(change.map(p =>{return `<@${p.id}> ${p.elo} => ${p.newElo}`}).join('\n'));
+    }
 }
 
 
@@ -113,7 +235,12 @@ function runQuery(query){
 }
 
 module.exports.register = register;
-//module.exports.unreg = unreg;
+module.exports.getMatchSides = getMatchSides;
+module.exports.updateRating = updateRating;
+module.exports.commit = commit;
+module.exports.canPlay = canPlay;
 module.exports.regMatch = regMatch;
 module.exports.getPlayerCurrentMatch = getPlayerCurrentMatch;
+module.exports.getStats = getStats;
+module.exports.recalcRating = recalcRating;
 
